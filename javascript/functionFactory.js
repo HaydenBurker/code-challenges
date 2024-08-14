3f0642bcfec86a5a4141585e1125f180e56a85f4
Hayden Burker <hayden.burker@gmail.com>
2024-08-13 19:18:52 -0600

    function factory challenge

---
+++ javascript/functionFactory.js
@@ -0,0 +1,17 @@
+// Create a function that takes a number n and returns a function which adds n to the number passed to it.
+
+function makeAddFunction(n) {
+    return function(n2) {
+        return n + n2;
+    }
+}
+
+const addFive = makeAddFunction(5);
+
+console.log(addFive(10)); // 15
+console.log(addFive(-5)); // 0
+
+const addTwelve = makeAddFunction(12);
+
+console.log(addTwelve(12)); // 24
+console.log(addTwelve(30)); // 42
