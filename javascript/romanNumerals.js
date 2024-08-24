d453afa082118fd516d585482db4ddf7926951ca
Hayden Burker <hayden.burker@gmail.com>
2024-08-23 18:02:35 -0600

    valid roman numeral challenge

--- javascript/romanNumerals.js
+++ javascript/romanNumerals.js
@@ -18,6 +18,7 @@ console.log(intToRoman(444));
 console.log(intToRoman(3765));
 console.log(intToRoman(3));
 console.log(intToRoman(34));
+console.log(intToRoman(1888))
 
 function romanToInt(roman) {
     const numbers = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000};
@@ -31,3 +32,23 @@ console.log(romanToInt("CDXLIV"));
 console.log(romanToInt("MMMDCCLXV"));
 console.log(romanToInt("III"));
 console.log(romanToInt("XXXIV"));
+
+
+// Create a function that checks if a roman numeral is valid
+
+function isValidRoman(roman) {
+    roman = roman.toUpperCase();
+    return Array.from({length: 3999}).map((_, i) => intToRoman(i + 1)).some(r => r === roman);
+}
+
+console.log(isValidRoman("Hello")); // false
+console.log(isValidRoman("I")); // true
+console.log(isValidRoman("i")); // true
+console.log(isValidRoman("MMMCMXCIX")); // true
+console.log(isValidRoman("MMMCMXCX")); // false
+console.log(isValidRoman("IIX")); // false
+console.log(isValidRoman("IX")); // true
+console.log(isValidRoman("XIII")); // true
+console.log(isValidRoman("XIIII")); // false
+console.log(isValidRoman("MDCCCLXXXVIII")); // true
+console.log(isValidRoman("IM")); // false
