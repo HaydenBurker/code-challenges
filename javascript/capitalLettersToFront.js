9ae5e501411be521ed47b38d2876b4f3cf9eaa44
Hayden Burker <hayden.burker@gmail.com>
2024-08-24 22:37:10 -0600

    capital letters to front challenge

---
+++ javascript/capitalLettersToFront.js
@@ -0,0 +1,13 @@
+// Create a function that moves all capital letters to the front of a word.
+
+function capToFront(letters) {
+    let capitalLetters = "";
+    let otherLetters = "";
+
+    letters.split("").forEach(l => /[A-Z]/.test(l) ? capitalLetters += l : otherLetters += l);
+    return capitalLetters + otherLetters;
+}
+
+console.log(capToFront("Hello World!")); // "HWello orld!"
+console.log(capToFront("raceCAR")); // "CARrace"
+console.log(capToFront("aBcDeFg")); // "BDFaceg"
