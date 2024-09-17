bacb42854ecd36ef09e3b5eba8c164db495416c8
Hayden Burker <hayden.burker@gmail.com>
2024-09-16 22:07:41 -0600

    length of number challenge

---
+++ python/length_of_number.py
@@ -0,0 +1,7 @@
+def length_of_number(num):
+    return len(str(num))
+
+print(length_of_number(10)) # 2
+print(length_of_number(0)) # 1
+print(length_of_number(2000)) # 4
+print(length_of_number(123456789)) # 9
