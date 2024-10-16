3c9cdc07b9f14d08389b4f6e003b38eed7089a57
Hayden Burker <hayden.burker@gmail.com>
2024-10-15 21:24:00 -0600

    automorphic numbers challenge

---
+++ javascript/automorphicNumbers.js
@@ -0,0 +1,14 @@
+// Create a function that returns true if a number is authmorphic and false otherwise
+// A number is automorphic if n is in the end of n^2
+
+function isAutomorphic(num) {
+    const numStr = num.toString();
+    const numSquaredStr = (num * num).toString()
+    return numStr === numSquaredStr.slice(numSquaredStr.length - numStr.length);
+}
+
+console.log(isAutomorphic(6)); // true
+console.log(isAutomorphic(3)); // false
+console.log(isAutomorphic(625)); // true
+console.log(isAutomorphic(100)); // false
+console.log(isAutomorphic(0)); // true
