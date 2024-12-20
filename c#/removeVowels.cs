a90264a85454cdc577d5e1f58036a447b440a14c
Hayden Burker <hayden.burker@gmail.com>
2024-12-19 22:05:01 -0700

    code formatting

--- c#/removeVowels.cs
+++ c#/removeVowels.cs
@@ -1,14 +1,15 @@
 // Create a function that takes in a string and returns the string without the vowels (aeiou)
 
 using System;
-					
+
 public class Program
 {
 	public static String RemoveVowels(String str)
 	{
 		String noVowelStr = "";
-		
-		foreach(char c in str) {
+
+		foreach(char c in str)
+    {
 			if (!"aeiou".Contains(c.ToString().ToLower()))
 			{
 				noVowelStr += c;	
@@ -17,10 +18,10 @@ public class Program
 
 		return noVowelStr;
 	}
-	
+
 	public static void Main()
 	{
-		Console.WriteLine(RemoveVowels("Hello world!")); // "Hll wrld"
+	  Console.WriteLine(RemoveVowels("Hello world!")); // "Hll wrld"
 		Console.WriteLine(RemoveVowels("AEioU")); // ""
 		Console.WriteLine(RemoveVowels("The quick brown fox jumps over the lazy dog")); // "Th qck brwn fx jmps vr th lzy dg"
 	}
