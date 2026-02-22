f9dad53436915e7e2ef91d119bcd25c14257877b
Hayden Burker <hayden.burker@gmail.com>
2026-02-21 23:53:10 -0700

    decimal to binary challenge

---
+++ python/decimal_to_binary.py
@@ -0,0 +1,16 @@
+# Create a function that takes an integer and converts it to a binary number
+
+def decimal_to_binary(decimal):
+    binary = []
+    while True:
+        binary.append(decimal % 2)
+        decimal //= 2
+        if decimal == 0:
+            break
+    zeros = [0] * max(0, 8 - len(binary))
+    return zeros + list(reversed(binary))
+
+print(decimal_to_binary(0)) # [0, 0, 0, 0, 0, 0, 0, 0]
+print(decimal_to_binary(255)) # [1, 1, 1, 1, 1, 1, 1, 1]
+print(decimal_to_binary(173)) # [1, 0, 1, 0, 1, 1, 0, 1]
+print(decimal_to_binary(100)) # [0, 1, 1, 0, 0, 1, 0, 0]
